2be186226eb1a8e685dd3237236510591b429dbe
Julien KUNTZMANN <jukuntzm@e1r13p11.42.fr>
2018-04-11 18:24:26 +0200

    process

--- 21sh/srcs/execve.c
+++ 21sh/srcs/execve.c
@@ -6,7 +6,7 @@
 /*   By: jukuntzm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/26 09:55:59 by jukuntzm          #+#    #+#             */
-/*   Updated: 2018/04/10 15:05:20 by jukuntzm         ###   ########.fr       */
+/*   Updated: 2018/04/11 18:24:09 by jukuntzm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,37 +60,47 @@ int		ft_exec(t_tree *tree, int pfd[])
 	i = 0;
 	if (!tree)
 		return (2);
-	if (pipe(pfd) == -1)
-	{
-		ft_putendl_fd("error pipe de merde", 2);
-		exit(EXIT_FAILURE);
-		// exit 13 (141)
+	/* 	if (pipe(pfd) == -1)
+		{
+	*/	ft_putendl_fd("error pipe de merde", 2);
+	/*	exit(EXIT_FAILURE);
+	// exit 13 (141)
 	}
-/*	if (tree->left)
-		i = ft_exec(tree->left, npfd);
+	if (tree->left)
+	i = ft_exec(tree->left, npfd);
 	if (tree->right)
-		i = ft_exec(tree->right, npfd);
+	i = ft_exec(tree->right, npfd);
 	if (ft_issep(tree->cmd[0]))
-		ft_sep(tree);
+	ft_sep(tree);
 	else
 	{
-*/	
+	*/	
 	close(pfd[0]);
+	dup2(pfd[1], STDOUT_FILENO);
+	close(pfd[1]);
 	if ((path = ft_getpath(tree->cmd)) == NULL)
-			return (-1);
-		arv = ft_strsplit(tree->args, 59);
-		if ((cpid = fork()) == -1)
-		{
-			ft_putendl_fd("error fork", 2);
-			exit (EXIT_FAILURE);
-			// exit 6? (?)
-		}
-		else if (cpid == 0)
-		{
-			i = execve(path, arv, environ);
-		}
-		close(pfd[1]);
-		waitpid(cpid, &i, WEXITSTATUS(i));
-//	}
+		return (-1);
+	arv = ft_strsplit(tree->args, 59);
+	cpid = fork();
+	if (cpid == -1)
+	{
+		ft_putendl_fd("error fork", 2);
+		exit (EXIT_FAILURE);
+		// exit 6? (?)
+	}
+	else
+	{
+//	ft_putendl_fd(path, 2);
+		i = execve(path, arv, environ);
+//	ft_putnbr_fd(i, 2);
+//	ft_putendl_fd("i size", 2);
+	}
+	close(pfd[1]);
+//	ft_putendl_fd("cpid ", 2);
+//	ft_putnbr_fd(cpid, 2);
+//	write(2, "\n", 1);
+	waitpid(cpid, &i, WEXITSTATUS(i));
+		ft_putendl_fd("error pipe", 2);
+	//	}
 	return (i);
 }
--- 21sh/srcs/execve2.c
+++ 21sh/srcs/execve2.c
@@ -6,7 +6,7 @@
 /*   By: jukuntzm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/28 12:02:19 by jukuntzm          #+#    #+#             */
-/*   Updated: 2018/04/10 15:05:20 by jukuntzm         ###   ########.fr       */
+/*   Updated: 2018/04/11 18:24:09 by jukuntzm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,39 @@ int		ft_newarg(t_tree *tree, int i, int pfd[])
 	char	*tmp;
 	int		l;
 
-//	close(pfd[1]);
-//	tmp = NULL;
-//		ft_putendl_fd("a", 2);
-//	ft_putnbr_fd(pfd[0], 2);
+	close(pfd[1]);
+	dup2(pfd[0], STDIN_FILENO);
+	//	close(pfd[1]);
+	//	tmp = NULL;
+	//		ft_putendl_fd("a", 2);
+	//	ft_putnbr_fd(pfd[0], 2);
 	while ((l = get_next_line(pfd[0], &tmp)) > 0)
 	{
 		tree->args = ft_strjoinfree(tree->args, ";", 1);
 		tree->args = ft_strjoinfree(tree->args, tmp, 3);
-	//	tmp = NULL;
+		//	tmp = NULL;
 	}
-//	ft_putnbr_fd(l, 2);
+	close(pfd[0]);
+	//	ft_putnbr_fd(l, 2);
+	return (i);
+}
+
+static int	ft_exec_pipe(t_tree *tree, int i, int pfd[])
+{
+	int	npfd[2];
+	char	*buf;
+	if ((pipe(npfd) == -1) || !(buf = (char *)malloc(sizeof(char) * 11)))
+		return (1);
+//	ft_putendl_fd("a", 2);
+	i = ft_sep(tree->left, i, npfd);
+	i = ft_newarg(tree, i, npfd);
+/*	if (pipe(pfd) == -1)
+		return (1); 
+	ft_putendl_fd("a", 2);
+*/	i = ft_exec(tree, npfd);
+	ft_putendl_fd("sfs", 2);
+		while (read(pfd[0], buf, 10) > 0)
+			ft_putstr_fd(buf, 1);
 	return (i);
 }
 
@@ -41,50 +63,54 @@ int		ft_sep(t_tree *tree, int i, int pfd[])
 		return (i);
 	if (!ft_strcmp(tree->cmd, ";"))
 	{
-//		ft_putendl_fd(" rentre point virgule", 2);
+		//		ft_putendl_fd(" rentre point virgule", 2);
 		i = ft_sep(tree->left, i, npfd);
 		i = ft_sep(tree->right, i, npfd);
 	}
 	else if (!ft_strcmp(tree->cmd, "&&"))
 	{
-//		ft_putendl_fd(" rentre double eper", 2);
+		//		ft_putendl_fd(" rentre double eper", 2);
 		i = ft_sep(tree->left, i, npfd);
 		if (i == 0)
-		i = ft_sep(tree->right, i, npfd);
+			i = ft_sep(tree->right, i, npfd);
 		else
 			return (i);
 	}
 	else if (!ft_strcmp(tree->cmd, "||"))
 	{
-//		ft_putendl_fd(" rentre double pipe", 2);
+		//		ft_putendl_fd(" rentre double pipe", 2);
 		i = ft_sep(tree->left, i, npfd);
 		if (i > 0)
-		i = ft_sep(tree->right, i, npfd);
+			i = ft_sep(tree->right, i, npfd);
 		else
 			return (i);
 	}
 	else if (!tree->left && !tree->right)
 	{
-//		ft_putendl_fd(" rentre exec", 2);
+		//		ft_putendl_fd(" rentre exec", 2);
 		i = ft_exec(tree, pfd);
 		while (read(pfd[0], buf, 10) > 0)
 			ft_putstr_fd(buf, 1);
 	}
 	else
 	{
-//		ft_putendl_fd(" rentre autre", 2);
-		i = ft_sep(tree->left, i, npfd);
-		i = ft_newarg(tree, i, npfd);
-		i = ft_exec(tree, npfd);
-		while (read(npfd[0], buf, 10) > 0)
-			ft_putstr_fd(buf, 1);
-	/*	i = ft_sep(tree->right, i, npfd);
-		if (tree->right)
-		{
-			i = ft_exec(tree, npfd);
-		while (read(npfd[0], buf, 10) > 0)
-			ft_putstr_fd(buf, 1);
-		}*/
+		i = ft_exec_pipe(tree, i, npfd);
+		/*		ft_putendl_fd(" rentre autre", 2);
+				dup2(npfd[1], STDOUT_FILENO);
+				i = ft_sep(tree->left, i, npfd);
+				close(npfd[0]);
+				dup2(npfd[1], STDIN_FILENO);
+				i = ft_newarg(tree, i, npfd);
+				i = ft_exec(tree, npfd);
+				while (read(npfd[0], buf, 10) > 0)
+				ft_putstr_fd(buf, 1);
+				i = ft_sep(tree->right, i, npfd);
+				if (tree->right)
+				{
+				i = ft_exec(tree, npfd);
+				while (read(npfd[0], buf, 10) > 0)
+				ft_putstr_fd(buf, 1);
+				}*/
 	}
 	return (i);
 }
