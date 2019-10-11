e26c1f2b5d372d7374545ecdb671c486e72def46
Julien KUNTZMANN <jukuntzm@e1r13p14.42.fr>
2019-10-11 12:50:45 +0200

    show_aloc_mem done

--- ft_malloc/test.c
+++ ft_malloc/test.c
@@ -8,130 +8,16 @@ void    ft_putchar(char c)
     write(1, &c, 1);
 }
 
-size_t	ft_strlen(const char *s)
+void    print_hex(unsigned char value, int index)
 {
-	size_t	i;
-
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	int		is1;
-	int		is2;
-	char	*str;
-
-	if (s1 && s2)
-	{
-		is1 = ft_strlen(s1);
-		is2 = ft_strlen(s2);
-		if (!(str = (char *)malloc((sizeof(char) * (is1 + is2)) + 1)))
-			return (NULL);
-		is1 = -1;
-		is2 = 0;
-		while (s1[++is1] != '\0')
-			str[is1] = s1[is1];
-		while (s2[is2] != '\0')
-		{
-			str[is1] = s2[is2];
-			is1++;
-			is2++;
-		}
-		str[is1] = '\0';
-		return (str);
-	}
-	return (NULL);
-}
-void	ft_putstr(char const *s)
-{
-	unsigned int	i;
-
-	if (s)
-	{
-		i = 0;
-		while (s[i] != '\0')
-		{
-			ft_putchar(s[i]);
-			i++;
-		}
-	}
-}
-void	ft_putnbr(int n)
-{
-	if (n == (-2147483648))
-	{
-		ft_putstr("-2147483648");
-		return ;
-	}
-	if (n < 0)
-	{
-		n = n * (-1);
-		ft_putchar('-');
-	}
-	if (n < 10)
-	{
-		ft_putchar(n + 48);
-		return ;
-	}
-	ft_putnbr(n / 10);
-	ft_putchar((n % 10) + 48);
-	return ;
-}
-void    ft_rev(char *str)
-{
-    int     i;
-    char    *tmp;
-    int     j;
-
-    i = 0;
-    j = 8;
-        write(1, "\n*****\n", 6);
-//    ft_putstr(str);
-    ft_putnbr(ft_strlen(str));
-        write(1, "\n", 1);
-    tmp = (char *)malloc(sizeof(ft_strlen(str)));
-    while ( j >= 0){
-        tmp[i++] = str[j++];
-        tmp[i++] = str[j];
-        i++;
-        j = j - 4;
-    }
-    ft_putstr(tmp);
-}
-
-void    *ft_memset(void *b, int c, size_t len)
-{
-	char		*mem;
-	size_t		i;
-
-	mem = b;
-	i = 0;
-	while (i < len)
-	{
-		mem[i] = c;
-		i++;
-	}
-	return (mem);
-}
-
-void    print_hex(unsigned char value, char **str,int size, int index)
-{
-    char c;
-
-    c = 'a';
     if (index < 2)
     {
-        print_hex(value / 16, str, size, index + 1);
+        print_hex(value / 16, index + 1);
         if (value % 16 >= 10) {
-            c = ('a' + value % 16 % 10);
-            *str = ft_strjoin(*str, &c);
+            ft_putchar('a' + value % 16 % 10);
         }
-        else {
-            c = ('0' + value % 16 % 10);
-            *str = ft_strjoin(*str, &c);
+        else if (value % 16 >= 0){
+            ft_putchar('0' + value % 16);
         }
     }
 }
@@ -140,27 +26,18 @@ int    print_memory(const void *addr, size_t size)
 {
     char    *ptr;
     size_t  i;
-    char    *str;
-    int  j;
 
-    if (!(str = (char *)malloc(sizeof(1))))
-        return 1;
-    str[0] = '\0';
-//    str = ft_memset(str, 0, 10);
     if (addr && size > 0)
     {
       ptr = (char *)addr;
-        i = 0;
-        j = 0;
-        write(1,"0x7f", 4);
-        while (i < size)
+        i = size;
+        write(1,"0x", 2);
+        while (i > 0)
         {
-            print_hex(*(ptr + i),&str, j, 0);
-            i++;
-            j = j + 2;
+            print_hex(ptr[i], 0);
+            i--;
         }
-        write(1, "\n", 1);
-        ft_rev(str);
+        print_hex(ptr[0], 0);
     }
     return 0;
 }
