eed4f5841ea83d81cce7ffff43ec07858a7ef5da
Julien KUNTZMANN <jukuntzm@e1r13p14.42.fr>
2019-10-12 15:53:23 +0200

    d

--- ft_malloc/includes/ft_malloc.h
+++ ft_malloc/includes/ft_malloc.h
@@ -6,14 +6,21 @@
 /*   By: jukuntzm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 09:30:34 by jukuntzm          #+#    #+#             */
-/*   Updated: 2019/09/19 10:29:02 by jukuntzm         ###   ########.fr       */
+/*   Updated: 2019/10/12 15:52:02 by jukuntzm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/mman.h>
-#include "../libft/libft.h"
+#include "libft/libft.h"
+
+typedef struct      s_page
+{
+    int             len;
+    char            *str;
+    struct  s_page  *next;
+}                   t_page;
 
 void	free(void *ptr);
 void	*malloc(size_t size);
--- ft_malloc/srcs/show_alloc_mem.c
+++ ft_malloc/srcs/show_alloc_mem.c
@@ -6,11 +6,54 @@
 /*   By: jukuntzm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/03 14:49:05 by jukuntzm          #+#    #+#             */
-/*   Updated: 2019/10/03 15:25:08 by jukuntzm         ###   ########.fr       */
+/*   Updated: 2019/10/12 15:52:02 by jukuntzm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_malloc.h"
+
+void    print_hex(unsigned char value, int index)
+{
+    if (index < 2)
+    {
+        print_hex(value / 16, index + 1);
+        if (value % 16 >= 10) {
+            ft_putchar('a' + value % 16 % 10);
+        }
+        else if (value % 16 >= 0){
+            ft_putchar('0' + value % 16);
+        }
+    }
+}
+
+int    print_memory(const void *addr, size_t size)
+{
+    char    *ptr;
+    size_t  i;
+
+    if (addr && size > 0)
+    {
+      ptr = (char *)addr;
+        i = size;
+        write(1,"0x", 2);
+        while (i > 0)
+        {
+            print_hex(ptr[i], 0);
+            i--;
+        }
+        print_hex(ptr[0], 0);
+    }
+    return 0;
+}
+
+void    print(t_page *mem){
+        print_memory(Tiny->str[0], 1);
+        ft_putstr(" - ");
+        print_memory(Tiny->str[Tiny->len], 1);
+        ft_putstr(" : ");
+        ft_putnbr(Tiny->len);
+        ft_putendl(" octets");
+}
 
 void    show_alloc_mem()
 {
@@ -20,10 +63,23 @@ void    show_alloc_mem()
 
     save_struct(Tiny, Small, Large, 1);
     ft_putstr("TINY : ");
+
     while(Tiny->next != NULL){
-        ft_putendl();
+        print(Tiny);
+        Tiny = Tiny->next;
     }
+    print(Tiny);
     ft_putstr("SMALL : ");
+    while(Small->next != NULL){
+        print(Small);
+        Small = Small->next;
+    }
+    print(Small);
     ft_putstr("LARGE : ");
+    while(Large->next != NULL){
+        print(Large);
+        Large = Large->next;
+    }
+    print(Large);
     
 }
